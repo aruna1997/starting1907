db8e69ba3685f67fd0a6c6594eb1e5cb1e705f75
Arun Aggarwal <arunaggarwal19077@gmail.com>
2017-10-07 13:26:20 +0530

    first

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express =  require('express');
 
-
+app.set('port', (process.env.PORT || 5000))
 var app = express();
 app.get('/', function(req,res) {
-	 res.sendFile('index.html');
+	 res.sendFile('index.html', {root: __dirname })
 });
