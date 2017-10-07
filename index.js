8b354a6b6f261e8a95515ba7ae91532ea3b87d16
Arun Aggarwal <arunaggarwal19077@gmail.com>
2017-10-07 13:35:27 +0530

    first

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var express =  require('express');
 
-app.set('port', (process.env.PORT || 5000));
+
 var app = express();
 app.get('/', function(req,res) {
 	 res.sendFile('index.html', {root: __dirname })
 });
 
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
